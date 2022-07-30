1eade8b4f463c0a4c91942a6c13cc9154f4b9372
Ranco Xu <rancoxu@gmail.com>
2022-07-30 15:35:44 +1000

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,25 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    text=models.CharField(max_length=200)
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # return string representation of the model
+        return self.text
+
+class Entry(models.Model):
+    # something specific learned about a Topic above
+    topic=models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text=models.TextField()
+    data_added=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # this class redefines the meta data of this model class
+        # verbose_name_plural tells Django the plural of Entry is Entries rather than Entrys
+        verbose_name_plural='entries'
+    
+    def __str__(self):
+        return f"{self.text[:50]}..."

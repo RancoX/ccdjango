f0797e15821a890f6befd70bc4aa28dfb7c67122
Ranco Xu <rancoxu@gmail.com>
2022-08-01 21:56:46 +1000

    new topic and entry func done

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -20,3 +22,49 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    '''
+    The new_topic() view handles two scenarios:
+
+    scenario 1: first time in new_topic
+        display an empty TopicForm
+
+    scenario 2: user filled out empty forms and submited it: request.method == 'POST' is the distinguisher
+    '''
+    if request.method != 'POST':
+        # No data submitted, so first time on this page, show blank forms
+        form = TopicForm()
+    else:
+        # POST method was detected
+        form = TopicForm(data=request.POST)
+        # data validation
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # display an empty form or invalid form if form.is_valid() evaluated to False
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic=Topic.objects.get(id=topic_id)
+
+    # if GET method, generate empty form then pass it to template
+    if request.method == 'GET':
+        form = EntryForm()
+    # if POST method, save the form and redirects to topic_<topic_id>
+    elif request.method == 'POST':
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry=form.save(commit=False)
+            new_entry.topic=topic
+            new_entry.save()
+            reverse_urlpattern = reverse('learning_logs:topic', kwargs={'topic_id': topic_id})
+            print(reverse_urlpattern)
+            # alternatively return redirect('learning_logs:topic', topic_id=topic_id)
+            return redirect(reverse_urlpattern)
+    context = {'form': form, 'topic':topic}
+    return render(request, 'learning_logs/new_entry.html', context)
